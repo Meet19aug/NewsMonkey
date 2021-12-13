4a5b16e5831a85172fd6eaef7dddb63dd0263d1c
Meet Patel <meetpatel19aug@gmail.com>
2021-12-13 10:38:50 +0530

    Remaining to implement infinite scrol in app Complete Working.

--- src/components/News.js
+++ src/components/News.js
@@ -6,6 +6,7 @@ import PropTypes from 'prop-types'
 
 
 export class News extends Component {
+    apiKey = "ebfbd906995d450291226ee186a8333d"
     static defaultProps = {
         country: 'in',
         pageSize: 8,
@@ -16,9 +17,11 @@ export class News extends Component {
         pageSize: PropTypes.number,
         category: PropTypes.string,
     }
-
-    constructor() {
-        super()
+    capitalizeFirstLetter(string) {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+      }
+    constructor(props) {
+        super(props)
         // console.log("Hello I am News Components constructor.")
         this.state = {
             articles: [],
@@ -26,20 +29,21 @@ export class News extends Component {
             page: 0,
             totalResults: 0,
         }
+        document.title = `${this.capitalizeFirstLetter(this.props.category)} - NewsMonkey`
     }
 
     async updateNews() {
-        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=a76371df11eb4bb2b06c9f91ca8063da&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.apiKey}&page=${this.state.page}&pageSize=${this.props.pageSize}`;
         this.setState({ loading: true });
         let data = await fetch(url);
         let parseData = await data.json();
         // console.log(parseData);
-        this.setState({ articles: parseData.articles, totalResults: parseData.totalResults, loading: false, })
+        this.setState({ articles: parseData.articles, totalResults: parseData.totalResults, loading: false})
     }
 
     async componentDidMount() {
         // console.log("componentDidMount function.")
-        // let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=a76371df11eb4bb2b06c9f91ca8063da&page=1&pageSize=${this.props.pageSize}`;
+        // let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.apiKey}&page=1&pageSize=${this.props.pageSize}`;
         // this.setState({loading: true});
         // let data = await fetch(url);
         // let parseData =  await data.json();
@@ -51,7 +55,7 @@ export class News extends Component {
 
     handlePrevClick = async () => {
         // console.log("previous");
-        // let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=a76371df11eb4bb2b06c9f91ca8063da&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
+        // let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.apiKey}&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
         // this.setState({loading: true});
         // let data = await fetch(url);
         // let parseData =  await data.json();
@@ -73,7 +77,7 @@ export class News extends Component {
         //     // console.log("running if part.<",Math.ceil(this.state.totalResults/${this.props.pageSize}) );
         // // }else{
         //     // console.log("running else part <", this.state.page+1, ">  < --total limit ", Math.ceil(this.state.totalResults/${this.pageSize}));
-        //     let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=a76371df11eb4bb2b06c9f91ca8063da&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
+        //     let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.apiKey}&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
         //     this.setState({loading: true});
         //     let data = await fetch(url);
         //     let parseData =  await data.json();
@@ -91,7 +95,7 @@ export class News extends Component {
     render() {
         return (
             <div className="container my-4">
-                <h1 className='text-center my-4' style={{ margin: '35px', }}>News-Monkey Top Headlines.</h1>
+                <h1 className='text-center my-4' style={{ margin: '35px', }}>News-Monkey Top {this.capitalizeFirstLetter(this.props.category)} Headlines.</h1>
                 {this.state.loading && <Spinner />}
                 <div className="row">
                     {!this.state.loading && this.state.articles.map((element) => {
